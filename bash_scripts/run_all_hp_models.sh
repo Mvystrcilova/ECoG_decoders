d5d0e4a21f8565bb3691659bc887ce9e1072c52d
Michaela Vystrcilova <Mvystrcilova1@seznam.cz>
2021-09-02 16:17:32 +0200

    predicting one model predictions with other model

--- bash_scripts/run_all_hp_models.sh
+++ bash_scripts/run_all_hp_models.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
 python training.py --kernel_size 1 1 1 1 --dilations 1 1 1 1 --variable 1 --starting_patient_index 1 --high_pass_train True --model_string='hp_m_'
+python training.py --kernel_size 1 1 1 1 --dilations 1 1 1 1 --variable 0 --starting_patient_index 1 --high_pass_train True --model_string='hp_m_'
+python training.py --kernel_size 2 2 2 2 --dilations 1 1 1 1 --variable 1 --starting_patient_index 1 --high_pass_train True --model_string='hp_m_'
+python training.py --kernel_size 2 2 2 2 --dilations 1 1 1 1 --variable 0 --starting_patient_index 1 --high_pass_train True --model_string='hp_m_'
+python training.py --kernel_size 3 3 3 3 --dilations 1 1 1 1 --variable 1 --starting_patient_index 1 --high_pass_train True --model_string='hp_m_'
+python training.py --kernel_size 3 3 3 3 --dilations 1 1 1 1 --variable 0 --starting_patient_index 1 --high_pass_train True --model_string='hp_m_'
